3529b39c346aa093e2f27ceae689a8358384773c
Sven Meyer <git_dev@svenmeyer.com>
2022-04-18 23:14:02 +1000

    test: parameter for shouldBehaveLikeGreeter - does not work

--- test/greeter/Greeter.behavior.ts
+++ test/greeter/Greeter.behavior.ts
@@ -1,8 +1,8 @@
 import { expect } from "chai";
 
-export function shouldBehaveLikeGreeter(): void {
+export function shouldBehaveLikeGreeter(parameter: string): void {
   it("should return the new greeting once it's changed", async function () {
-    expect(await this.greeter.connect(this.signers.admin).greet()).to.equal("Hello, world!");
+    expect(await this.greeter.connect(this.signers.admin).greet()).to.equal(parameter);
 
     await this.greeter.setGreeting("Bonjour, le monde!");
     expect(await this.greeter.connect(this.signers.admin).greet()).to.equal("Bonjour, le monde!");
--- test/greeter/Greeter.ts
+++ test/greeter/Greeter.ts
@@ -6,6 +6,8 @@ import type { Greeter } from "../../src/types/Greeter";
 import { Signers } from "../types";
 import { shouldBehaveLikeGreeter } from "./Greeter.behavior";
 
+var testString: string;
+
 describe("Unit tests", function () {
   before(async function () {
     this.signers = {} as Signers;
@@ -19,8 +21,10 @@ describe("Unit tests", function () {
       const greeting: string = "Hello, world!";
       const greeterArtifact: Artifact = await artifacts.readArtifact("Greeter");
       this.greeter = <Greeter>await waffle.deployContract(this.signers.admin, greeterArtifact, [greeting]);
+
+      testString = "Hello, world!"; // does not work > undefined in shouldBehaveLikeGreeter
     });
 
-    shouldBehaveLikeGreeter();
+    shouldBehaveLikeGreeter(testString);
   });
 });
